3bac722c1ae5635797f35d40aff343e0b89a891b
Zi-Hao <1007823029@qq.com>
2018-12-04 18:18:00 +0800

    Explanation

---
+++ Explanation/scripts/displayCitations.js
@@ -0,0 +1,24 @@
+function displayCitiations(){
+    if(!document.getElementsByTagName || !document.createElement || !document.createTextNode) return false;//判断是否可用
+    //取得所有引用
+    var quotes = document.getElementsById("blockquote");
+    //遍历引用
+    for(var i=0;i<quotes.length;i++){
+        //如果没有cite属性，继续循环
+        if(!quotes[i].getAttribute("cite")) continue;
+        //保存cite属性
+        var url = quotes[i].getAttribute("cite");
+        //取得引用中的所有元素节点
+        var quoteChildren = quotes[i].getElementByTagName("*");
+        //如果没有元素节点，继续循环
+        if(quotesChildren.length<1) continue;
+        //取得引用中的最后一个元素节点
+        var elem = quoteChildren[quoteChildren.length - 1];
+        link.appendChild(link_text);
+        link.setAttribute("href",url);
+        var superscript = document.createElement("sup");
+        superscript.appendChild(link);
+        elem.appendChild(superscript);
+    }
+}
+addLoadEvent(displayCitations);
