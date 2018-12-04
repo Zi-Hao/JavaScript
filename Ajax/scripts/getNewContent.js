dfa8706ac762965eaadfbd6a51c44f13dac91552
Zi-Hao <1007823029@qq.com>
2018-12-04 17:57:43 +0800

    Ajax

---
+++ Ajax/scripts/getNewContent.js
@@ -0,0 +1,19 @@
+function getNewContent(){
+    var request = getHTTPObject();
+    if(request){
+        request.open("GET","example.txt",true);
+        request.onreadystatechange = function(){
+            if(request.readyState == 4){
+                var para = document.createElement("p");
+                var txt = document.createTextNode(request.responseText);
+                para.appendChild(txt);
+                document.getElementById('new').appendChild(para);
+            }
+        };
+        request.send('');
+        //alert('A');
+    }else{
+        alert('Sorry,your browser doesn\'t support XMLHttpRequest');
+    }
+}
+addLoadEvent(getNewContent);
