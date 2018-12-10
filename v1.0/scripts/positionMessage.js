a7688b46c16050574b25466db639d58e852aaca0
Zi-Hao <1007823029@qq.com>
2018-12-10 14:46:01 +0800

    Create message.html

---
+++ v1.0/scripts/positionMessage.js
@@ -0,0 +1,8 @@
+function positionMessage(){
+    if(!document.getElementById) return false;
+    if(!document.getElementById("message")) return false;
+    var elem = document.getElementById("message");
+    elem.style.position = "absolute";
+    elem.style.left = "50px";
+    elem.style.top = "100px";
+}
