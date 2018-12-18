c2f00609d254e3f6d348984035bf89ac59b53d5f
Zi-Hao <1007823029@qq.com>
2018-12-18 19:50:56 +0800

    Update positionMessage.js

--- v1.0/scripts/positionMessage.js
+++ v1.0/scripts/positionMessage.js
@@ -1,8 +1,37 @@
-function positionMessage(){
+// function positionMessage(){
+//     if(!document.getElementById) return false;
+//     if(!document.getElementById("message")) return false;
+//     var elem = document.getElementById("message");
+//     elem.style.position = "absolute";
+//     elem.style.left = "50px";
+//     elem.style.top = "100px";
+//     movement = setTimeout("moveMessage()",5000);
+// }
+
+function moveMessage(){
     if(!document.getElementById) return false;
     if(!document.getElementById("message")) return false;
     var elem = document.getElementById("message");
-    elem.style.position = "absolute";
-    elem.style.left = "50px";
-    elem.style.top = "100px";
+    const xpos = parseInt(elem.style.left);
+    const ypos = parseInt(elem.style.top);
+    if(xpos == 200 && ypos == 100){
+        return ture;
+    }
+    if(xpos<200){
+        xpos++;
+    }
+    if(xpos>200){
+        xpos--;
+    }
+    if(ypos<100){
+        ypos++;
+    }
+    if(ypos>100){
+        ypos--;
+    }
+    elem.style.left = xpos + "px";
+    elem.style.top = ypos + "px";
+    movement = setTimeout("moveMessage()",10);
 }
+// addLoadEvent(positionMessage);
+addLoadEvent(moveMessage);
