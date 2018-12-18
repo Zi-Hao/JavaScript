b92da1a8a69eda66341845bbf950223718fe1cae
Zi-Hao <1007823029@qq.com>
2018-12-18 20:59:07 +0800

    Update moveElement.js

--- v2.0/scripts/moveElement.js
+++ v2.0/scripts/moveElement.js
@@ -12,16 +12,20 @@ function moveElement(elementID,final_x,final_y,interval) {
         return true;
     }
     if (xpos < final_x) {
-        xpos++;
+        dist = Math.ceil((final_x-xpos)/10);
+        xpos = xpos + dist;
     }
     if (xpos > final_x) {
-        xpos--;
+        dist = Math.ceil((xpos-final_x)/10);
+        xpos = xpos - dist;
     }
     if (ypos < final_y) {
-        ypos++;
+        dist = Math.ceil((final_y-ypos)/10);
+        ypos = ypos + dist;
     }
     if (ypos > final_y) {
-        ypos--;
+        dist = Math.ceil((ypos-final_y)/10);
+        ypos = ypos - dist;
     }
     elem.style.left = xpos + "px";
     elem.style.top = ypos + "px";
